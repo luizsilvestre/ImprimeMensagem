9c4af87de977dc786bcbd25a0bc0babb1c0b3ea8
luiz silvestre silvestre <19095519-luiz-silvestres@users.noreply.replit.com>
2024-03-22 00:04:28 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,42 @@
+/*Programa imprime uma mensagem na tela
+Autor:Luiz Silvestre*/
+
+public class Main {
+  public static void main(String[] args) {
+    // System.out.println("Hello world!");
+
+
+    // Variaveis do tipo booleano
+    boolean media = true;
+    boolean frequencia = true;
+    boolean aprovado = media && frequencia;
+    boolean cafe = false;
+    boolean agua = true;
+    boolean clienteServido = cafe || agua;
+
+   
+    // Variavel do tipo inteiro
+
+    int idade = 20;
+
+    //Variavel do tipo real
+    double salario = 1250.75;
+    float temperatura = 25.8f;
+
+    //Variavel do tipo texto
+    String nome = "Luiz Silvestre";
+    String CPF = "123.456.789-85";
+    String CEP = "0123-456";
+    String aniversario = "01/01/1990";
+
+
+    //Metodo de saida
+
+    // System.out.println(" Cliente Servido?  " + clienteServido);
+
+    
+
+  }
+
+ 
+}
